75c6f1e555c6e58976c5d8499bc65e9a5def66ac
server <server>
2021-08-24 11:09:04 +0300

    some changes

--- main.py
+++ main.py
@@ -44,11 +44,13 @@ def compute_style_loss(P, F):
     F - Tensor. similar to F, but applied on the **generated** image.
     """
     
-    num_layers, batch_size, c, d,e= C.size()
-    C=C.view(num_layers,batch_size,c,d*e)
-    C=G.view(num_layers,batch_size,c,d*e)
-    w=torch.ones(num_layers)/5
+    num_layers, batch_size, c, d,e = C.size()
+    C = C.view(num_layers,batch_size,c,d*e)
+    C = G.view(num_layers,batch_size,c,d*e)
+    w=torch.ones(num_layers)/num_layers
+    w = w.to(C.device)
     loss = torch.zeros(batch_size)
+    loss = loss.to(C.device)
     for l in range(num_layers):
         p, f = P[l], F[l]
         a, g = gram_matrix(p), gram_matrix(f) #check with batch size > 1
@@ -62,14 +64,13 @@ def compute_layer_content_loss(C, G):
     C-  a genereted vector of tensor of batch size* num of filter* size of filter 
     G-  a given vector of tensor of batch size* num of filter* size of filter 
     """
-    num_layers, batch_size, c, d,e=C.size()
-    C=C.view(num_layers,batch_size,c,d*e)
-    G=G.view(num_layers,batch_size,c,d*e)
-    w=torch.ones(num_layers)/5
-    loss = torch.zeros(batch_size)
+    num_layers, batch_size, c, d, e = C.size()
+    C=C.view(num_layers,batch_size, c, d*e)
+    G=G.view(num_layers,batch_size, c, d*e)
+    loss = 0  #  torch.zeros(batch_size, device=C.device)
     for l in range(num_layers):
         p, f = C[l], G[l]
-        loss += w[l]* 1/2 * torch.linalg.norm(p-f,dim=(1,2)) ** 2# unsure if this is the formula 
+        loss += 1/2 * torch.linalg.norm(p-f,dim=(1,2)) ** 2  # unsure if this is the formula 
     return loss
          
 
@@ -77,20 +78,20 @@ def compute_content_loss(outputs, style_outputs, style_names, content_outputs, c
     """
 
     """
-    x_style =torch.stack([ outputs[key] for key in outputs.keys() if key in style_names])
-    x_con =torch.stack([ outputs[key]  for key in outputs.keys()  if key in content_names ] )
+    x_style =torch.stack([outputs[key] for key in outputs.keys() if key in style_names])
+    x_con =torch.stack([outputs[key]  for key in outputs.keys()  if key in content_names ] )
     y_style = torch.stack([ style_outputs[key] for key in style_outputs.keys()  if key in style_names ])
     y_con = torch.stack([ content_outputs[key] for key in content_outputs.keys() if key in content_names ])
 
     
-    return alpha*compute_layer_content_loss(x_con,y_con)+ beta*compute_layer_content_loss(x_style, y_style)
+    return alpha*compute_layer_content_loss(x_con,y_con) + beta*compute_layer_content_loss(x_style, y_style)
     
     
 
 
-def train(ephoch_num, input_size, style_image, content_image, alpha=1, beta=1):
+def train(ephoch_num, input_size, style_image, content_image, alpha=1e0, beta=1, device="cuda"):
 
-    inputs = torch.rand([1] + list(input_size), requires_grad=True)
+    inputs = torch.rand([1] + list(input_size), requires_grad=True, device=device)
 
     layers = ["conv1_1", "relu1_1", "conv1_2","relu1_2", "maxpool1",
                                                          "conv2_1", "relu2_1", "conv2_2", "relu2_2", "maxpool2",
@@ -100,7 +101,7 @@ def train(ephoch_num, input_size, style_image, content_image, alpha=1, beta=1):
                                                             "relu4_3", "conv4_4", "relu4_4", "maxpool4",
                                                         "conv5_1", "relu5_1", "conv5_2", "relu5_2", "conv5_3", 
                                                             "relu5_3", "conv5_4", "relu5_4", "maxpool5"]
-    model = vgg19(pretrained=True)
+    model = vgg19(pretrained=True).to(device)
     splitted_model = ByLayerModel(model.features, names=layers)
 
     for p in model.parameters():
@@ -114,8 +115,8 @@ def train(ephoch_num, input_size, style_image, content_image, alpha=1, beta=1):
     optimizer = torch.optim.Adam([inputs])
     criterion = compute_content_loss
     
-    style_names = ["conv1_1"]
-    content_names = ["conv1_1"]
+    style_names = ["conv1_1"]  # , "conv2_1", "conv3_1", "conv4_1"
+    content_names = ["conv4_2"]
 
     assert(set(style_names).issubset(set(layers)))
     assert(set(content_names).issubset(set(layers)))
@@ -126,26 +127,31 @@ def train(ephoch_num, input_size, style_image, content_image, alpha=1, beta=1):
     
     transform = transforms.Compose([transforms.Resize(input_size[1:]), normalize])
 
-    style_image = transform(style_image)
-    content_image = transform(content_image)
+    style_image = transform(style_image).to(device)
+    content_image = transform(content_image).to(device)
 
     
     style_outputs = splitted_model(style_image)
     content_outputs = splitted_model(content_image)
 
     for epcoh_num in trange(ephoch_num):
-    
+
         outputs = splitted_model(normalize(inputs))
         
         loss = criterion(outputs, style_outputs, style_names, content_outputs, content_names, alpha=alpha, beta=beta)
         
-        loss.backward()
+        loss.backward(retain_graph=True)
 
         loss_values.append(loss.item())
 
         optimizer.step()
         optimizer.zero_grad()
-
+        # before = torch.clone(inputs)
+        with torch.no_grad():
+            for i in inputs:
+                i.clamp_(0, 255)
+        # if before.equal(inputs):
+        #     raise RuntimeError
 
         
 
@@ -158,11 +164,11 @@ def train(ephoch_num, input_size, style_image, content_image, alpha=1, beta=1):
 
 
 if __name__ == "__main__":
-    EPOCH_NUM = 100
-    INPUT_SIZE = (3, 224, 224)
+    EPOCH_NUM = 2000
+    INPUT_SIZE = (3, 512, 512)
 
-    STYLE_IMAGE = "images/images/Francisco_Goya/Francisco_Goya_79.jpg"
-    CONTENT_IMAGE = "content/tom.jpg"
+    STYLE_IMAGE = "style_photos/Piet_Mondrian_32.jpg"
+    CONTENT_IMAGE = "content/tel_aviv.jpg"
 
     style_image = load_image(STYLE_IMAGE)
     content_image = load_image(CONTENT_IMAGE)
@@ -177,7 +183,7 @@ if __name__ == "__main__":
     # img = inputs.detach().cpu().numpy()
     img = transforms.ToPILImage()(inputs.squeeze(0))
     
-    img.save("output.png")
+    img.save("mondrian.png")
 
     
 
